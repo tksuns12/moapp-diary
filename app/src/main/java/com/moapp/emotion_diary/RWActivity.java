487aaae133b5fb0a382ee9cde2aa9dc938db99a9
tksuns12 <tksuns@gmail.com>
2019-12-09 20:32:39 +0900

    A startover

--- app/src/main/java/com/moapp/emotion_diary/RWActivity.java
+++ app/src/main/java/com/moapp/emotion_diary/RWActivity.java
@@ -32,14 +32,18 @@ public class RWActivity extends AppCompatActivity {
         setContentView(R.layout.activity_rw);
         textView = findViewById(R.id.date_picker);
         Calendar calendar = Calendar.getInstance();
+        // 오늘 연,월,일 불러옴
         int today_year = calendar.get(Calendar.YEAR);
         int today_month = calendar.get((Calendar.MONTH)+1);
         int today_date = calendar.get(Calendar.DATE);
         realm = Realm.getDefaultInstance();
+        //인텐트 불러옴
         Intent intent = getIntent();
+        //연월일을 인텐트에서 불러온대로 설정, 없으면 오늘 연월일을 기본값으로 설정
         mYear = Integer.toString(intent.getIntExtra("year", today_year));
         mMonth = Integer.toString(intent.getIntExtra("month", today_month));
         mDate = Integer.toString(intent.getIntExtra("date", today_date));
+        //연월일이 표시되어야 하는데 잘 작동하지 않는 것 같음.
         textView.setText(mYear + "년 " + mMonth + "월 " + mDate + "일");
 
     }
@@ -47,12 +51,14 @@ public class RWActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        //연월일이 표시되어야 하는데 잘 작동하지 않는 것 같음.
         textView.setText(mYear + "년 " + mMonth + "월 " + mDate + "일");
 
     }
 
     public void clickSave(View view) {
         editText = findViewById(R.id.content_view);
+        //저장 버튼 누를 시 새로운 DiaryData 타입의 데이터를 만든 후 연월일, 일기 내용 설정 후 데이터베이스에 저장
         realm.beginTransaction();
         DiaryData diaryData = realm.createObject(DiaryData.class, Integer.parseInt(mYear + mMonth + mDate));
         diaryData.setContent(editText.getText().toString());
@@ -64,13 +70,14 @@ public class RWActivity extends AppCompatActivity {
     }
 
     public void clickDatePicker(View view) {
+        //날짜를 클릭할 시 캘린더 대화창 띄움
         DatePickerDialog dialog = new DatePickerDialog(this, listener
                 , Integer.parseInt(mYear)
                 , Integer.parseInt(mMonth)-1
                 , Integer.parseInt(mDate));
         dialog.show();
     }
-
+    //날짜가 바뀔 시 연월일을 바뀐대로 설정, 그러나 잘 안 되는 것 같음.
     private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
@@ -80,6 +87,7 @@ public class RWActivity extends AppCompatActivity {
         }
     };
 
+    //삭제 버튼 누를 시 오늘 연월일에 해당하는 일기를 찾은 뒤 데이터베이스에서 삭제
     public void clickDelete(View view) {
         DiaryData data = realm.where(DiaryData.class).equalTo("uniqueKey",
                 Integer.parseInt(mYear+mMonth+mDate)).findFirst();
