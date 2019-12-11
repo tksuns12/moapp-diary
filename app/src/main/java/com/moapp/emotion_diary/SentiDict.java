4c54b4b8016593985c24cc82dc8e220aa6079101
tksuns12 <tksuns@gmail.com>
2019-12-11 18:37:39 +0900

    로딩 화면 구현(미려한 UI 추가 필요). NLP 관련 기능 구현 중(미완성).

---
+++ app/src/main/java/com/moapp/emotion_diary/SentiDict.java
@@ -0,0 +1,38 @@
+package com.moapp.emotion_diary;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class SentiDict extends RealmObject {
+
+    private int score;
+    private int gram;
+
+    @PrimaryKey
+    private String word;
+
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getGram() {
+        return gram;
+    }
+
+    public void setGram(int gram) {
+        this.gram = gram;
+    }
+}
